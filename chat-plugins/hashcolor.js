a4c5a40679f4736a237daa2a9687eaaeaa16eefa
Mystifi <bornfromthemyst@gmail.com>
2016-09-21 16:46:23 -0500

    Update hashcolor.js

--- chat-plugins/hashcolor.js
+++ chat-plugins/hashcolor.js
@@ -1,11 +1,22 @@
-var filepath = 'config/customcolors.json';
-var customColors = {};
-var fs = require('fs');
-var request = require('request');
-var path = require('path');
+/**
+ * Hashcolor file.
+ * 
+ * Credits go to:
+ * - panpawn and jd
+ */
+
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let https = require('https');
+let request = require('request');
+
+let customColors = {};
+const FILE_PATH = 'config/customcolors.json';
 
 function load () {
-        fs.readFile(filepath, 'utf8', function (err, file) {
+        fs.readFile(FILE_PATH, 'utf8', (err, file) => {
                 if (err) return;
                 customColors = JSON.parse(file);
         });
@@ -20,36 +31,44 @@ load();
        fs.appendFile(file, date + msg);
 }*/
 
+function getCSS() {
+        let options = {
+                host: 'play.pokemonshowdown.com',
+                port: 80,
+                path: '/customcss.php?server=spacialgaze',
+                method: 'GET',
+        };
+        https.get(options);
+}
+SG.loadCSS = getCSS;
 
 function updateColor() {
-        fs.writeFileSync(filepath, JSON.stringify(customColors));
+        fs.writeFileSync(FILE_PATH, JSON.stringify(customColors));
 
-        var newCss = '/* COLORS START */\n';
+        let newCss = '/* COLORS START */\n';
 
-        for (var name in customColors) {
-                newCss += generateCSS(name, customColors[name]);
+        for (let name in customColors) {
+                newCss += generateCSS(toId(name), customColors[name]);
         }
         newCss += '/* COLORS END */\n';
 
-        var file = fs.readFileSync('config/custom.css', 'utf8').split('\n');
+        let file = fs.readFileSync('config/custom.css', 'utf8').split('\n');
         if (~file.indexOf('/* COLORS START */')) file.splice(file.indexOf('/* COLORS START */'), (file.indexOf('/* COLORS END */') - file.indexOf('/* COLORS START */')) + 1);
         fs.writeFileSync('config/custom.css', file.join('\n') + newCss);
-        request('http://play.pokemonshowdown.com/customcss.php?server=skypillar&invalidate', function callback(error, res, body) {
-                if (error) return console.log('updateColor error: ' + error);
-        });
+
+        getCSS();
 }
-global.updateColor = updateColor;
+SG.updateColor = updateColor;
 
 function generateCSS(name, color) {
-        var css = '';
-        var rooms = [];
-        name = toId(name);
-        for (var room in Rooms.rooms) {
-                if (Rooms.rooms[room].id === 'global' || Rooms.rooms[room].type !== 'chat' || Rooms.rooms[room].isPersonal) continue;
-                rooms.push('#' + Rooms.rooms[room].id + '-userlist-user-' + name + ' strong em');
-                rooms.push('#' + Rooms.rooms[room].id + '-userlist-user-' + name + ' strong');
-                rooms.push('#' + Rooms.rooms[room].id + '-userlist-user-' + name + ' span');
-        }
+        let css = '';
+        let rooms = [];
+        Rooms.rooms.forEach(curRoom => {
+                if (curRoom.id === 'global' || curRoom.type !== 'chat' || curRoom.isPersonal) continue;
+                rooms.push('#' + curRoom.id + '-userlist-user-' + name + ' strong em');
+                rooms.push('#' + curRoom.id + '-userlist-user-' + name + ' strong');
+                rooms.push('#' + curRoom.id + '-userlist-user-' + name + ' span');
+        });
         css = rooms.join(', ');
         css += '{\ncolor: ' + color + ' !important;\n}\n';
         css += '.chat.chatmessage-' + name + ' strong {\n';
@@ -60,34 +79,35 @@ function generateCSS(name, color) {
 exports.commands = {
         customcolor: function (target, room, user) {
                 if (!this.can('hotpatch')) return false;
-                target = target.split(',');
-                for (var u in target) target[u] = target[u].trim();
-                if (!target[1]) return this.parse('/help customcolor');
-                if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
-                if (target[1] === 'delete') {
-                        if (!customColors[toId(target[0])]) return this.errorReply('/customcolor - ' + target[0] + ' does not have a custom color.');
-                        delete customColors[toId(target[0])];
+                target = target.split(',').map(param => param.trim());
+                if (target.length !== 2) return this.parse('/help customcolor');
+                let targetUser = target[0];
+                let option = target[1];
+                if (toId(targetUser).length > 18) return this.errorReply("Usernames cannot exceed 18 characters.");
+                if (option === 'delete') {
+                        if (!customColors[toId(targetUser)]) return this.errorReply('/customcolor - ' + targetUser + ' does not have a custom color.');
+                        delete customColors[toId(targetUser)];
                         updateColor();
-                        this.sendReply("You removed " + target[0] + "'s custom color.");
-                        if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup(user.name + " removed your custom color.");
+                        this.sendReply("You removed " + targetUser + "'s custom color.");
+                        if (Users(targetUser) && Users(targetUser).connected) Users(targetUser).popup(user.name + " removed your custom color.");
                         return;
                 }
 
-                this.sendReply("|raw|You have given <b><font color=" + target[1] + ">" + Tools.escapeHTML(target[0]) + "</font></b> a custom color.");
-                //logMoney(user.name + " le ha asignado un color personalizado a " + target[0] + ". (Color: " + target[1] + ").");
-                customColors[toId(target[0])] = target[1];
+                if (option.charAt(0) !== '#') return this.errorReply("The color needs to be a hex starting with '#'.");
+                this.sendReply("|raw|You have given <b><font color=" + option + ">" + Tools.escapeHTML(targetUser) + "</font></b> a custom color.");
+                // logMoney(user.name + " le ha asignado un color personalizado a " + target[0] + ". (Color: " + target[1] + ").");
+                customColors[toId(targetUser)] = option;
                 updateColor();
         },
         customcolorhelp: ["Commands Include:",
-                                "/customcolor [user], [hex] - Gives [user] a custom color of [hex]",
-                                "/customcolor [user], delete - Deletes a user's custom color"],
+                "/customcolor [user], [hex] - Gives [user] a custom color of [hex]",
+                "/customcolor [user], delete - Deletes a user's custom color"],
 
         cp: 'colorpreview',
         colorpreview: function (target, room, user) {
                 if (!this.canBroadcast()) return;
-                target = target.split(',');
-                for (var u in target) target[u] = target[u].trim();
-                if (!target[1]) return this.parse('/help colorpreview');
+                target = target.split(',').map(param => param.trim());
+                if (target.length !== 2) return this.parse('/help colorpreview');
                 return this.sendReplyBox('<b><font size="2" color="' +  target[1] + '">' + Tools.escapeHTML(target[0]) + '</font></b>');
         },
         colorpreviewhelp: ["Usage: /colorpreview [user], [color] - Previews what that username looks like with [color] as the color."],
@@ -100,23 +120,23 @@ exports.commands = {
  * based on the userid.
 */
 
-var MD5 = require('MD5');
-var colorCache = {};
+let MD5 = require('MD5');
+let colorCache = {};
 
 // hashColor function
 function hashColor(name) {
         name = toId(name);
         if (customColors[name]) return customColors[name];
         if (colorCache[name]) return colorCache[name];
-        var hash = MD5(name);
-        var H = parseInt(hash.substr(4, 4), 16) % 360; // 0 to 360
-        var S = parseInt(hash.substr(0, 4), 16) % 50 + 40; // 40 to 89
-        var L = Math.floor(parseInt(hash.substr(8, 4), 16) % 20 + 30); // 30 to 49
-        var C = (100 - Math.abs(2 * L - 100)) * S / 100 / 100;
-        var X = C * (1 - Math.abs((H / 60) % 2 - 1));
-        var m = L / 100 - C / 2;
+        let hash = MD5(name);
+        let H = parseInt(hash.substr(4, 4), 16) % 360; // 0 to 360
+        let S = parseInt(hash.substr(0, 4), 16) % 50 + 40; // 40 to 89
+        let L = Math.floor(parseInt(hash.substr(8, 4), 16) % 20 + 30); // 30 to 49
+        let C = (100 - Math.abs(2 * L - 100)) * S / 100 / 100;
+        let X = C * (1 - Math.abs((H / 60) % 2 - 1));
+        let m = L / 100 - C / 2;
 
-        var R1, G1, B1;
+        let R1, G1, B1;
         switch (Math.floor(H / 60)) {
                 case 1: R1 = X; G1 = C; B1 = 0; break;
                 case 2: R1 = 0; G1 = C; B1 = X; break;
@@ -125,25 +145,25 @@ function hashColor(name) {
                 case 5: R1 = C; G1 = 0; B1 = X; break;
                 case 0: default: R1 = C; G1 = X; B1 = 0; break;
         }
-        var lum = (R1 + m) * 0.2126 + (G1 + m) * 0.7152 + (B1 + m) * 0.0722; // 0.05 (dark blue) to 0.93 (yellow)
-        var HLmod = (lum - 0.5) * -100; // -43 (yellow) to 45 (dark blue)
+        let lum = (R1 + m) * 0.2126 + (G1 + m) * 0.7152 + (B1 + m) * 0.0722; // 0.05 (dark blue) to 0.93 (yellow)
+        let HLmod = (lum - 0.5) * -100; // -43 (yellow) to 45 (dark blue)
         if (HLmod > 12) HLmod -= 12;
         else if (HLmod < -10) HLmod = (HLmod + 10) * 2 / 3;
         else HLmod = 0;
 
         L += HLmod;
-        var Smod = 10 - Math.abs(50 - L);
+        let Smod = 10 - Math.abs(50 - L);
         if (HLmod > 15) Smod += (HLmod - 15) / 2;
         S -= Smod;
 
-        var rgb = hslToRgb(H, S, L);
+        let rgb = hslToRgb(H, S, L);
         colorCache[name] = "#" + rgbToHex(rgb.r, rgb.g, rgb.b);
         return colorCache[name];
 }
 global.hashColorWithCustoms = hashColor;
 
 function hslToRgb(h, s, l) {
-        var r, g, b, m, c, x;
+        let r, g, b, m, c, x;
         if (!isFinite(h)) h = 0;
         if (!isFinite(s)) s = 0;
         if (!isFinite(l)) l = 0;
