bcd6191c4ad62269e2172553cac648bb4959e8b0
HoeenHero <HoeenCoder@users.noreply.github.com>
2016-12-08 11:55:08 -0500

    Uno highlight fixes

--- chat-plugins/uno.js
+++ chat-plugins/uno.js
@@ -512,7 +512,7 @@ class Game {
 
 	initNextTurn() {
 		this.room.add(getUserName(this.player) + "'s turn.");
-		Users(toId(this.player)).sendTo(this.room, '|chat|~UnoManager|' + this.player + ' it\'s your turn.');
+		Users(toId(this.player)).sendTo(this.room, '|chat|~UnoManager|' + Users(toId(this.player)).name + ' it\'s your turn.');
 		this.room.update();
 		this.id++;
 		let user = Users.get(this.player);
