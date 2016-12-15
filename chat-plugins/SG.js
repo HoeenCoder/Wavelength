86bbb2dcb21e650ba9ac806806f785c39ab714f4
HoeenHero <HoeenCoder@users.noreply.github.com>
2016-12-15 13:13:23 -0500

    Basic stream reading

--- chat-plugins/SG.js
+++ chat-plugins/SG.js
@@ -109,6 +109,51 @@ SG.randomString = function (length) {
 	return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
 };
 
+/*
+SG.masterGameObj = {};
+//This code is a WIP
+SG.writeGameData = function () {
+	let buf = '';
+	for (let key in SG.masterGameObj) {
+		let obj = {};
+		obj[key] = SG.masterGameObj[key];
+		buf += JSON.stringify(obj) + '\n';
+	}
+	fs.writeFile('config/game.json', buf, 'utf-8');
+}
+
+SG.readGameData = function () {
+	var stream = fs.createReadStream('config/game.json', {flags: 'r', encoding: 'utf-8'});
+	var buf = '';
+
+	stream.on('data', function(d) {
+    		buf += d.toString(); // when data is read, stash it in a string buffer
+    		pump(); // then process the buffer
+	});
+
+	function pump() {
+    		var pos;
+
+    		while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+			if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
+	    			buf = buf.slice(1); // discard it
+	    			continue; // so that the next iteration will start with data
+			}
+			processLine(buf.slice(0,pos)); // hand off the line
+			buf = buf.slice(pos+1); // and slice the processed data off the buffer
+    		}
+	}
+
+	function processLine(line) { // here's where we do something with a line
+		if (line[line.length-1] == '\r') line=line.substr(0,line.length-1); // discard CR (0x0D)
+		if (line.length > 0) { // ignore empty lines
+			var obj = JSON.parse(line); // parse the JSON
+			Object.assign(SG.masterGameObj, obj); // do something with the data here!
+    		}
+	}
+}
+SG.readGameData();
+*/
 // last two functions needed to make sure SG.regdate() fully works
 function loadRegdateCache() {
 	try {
