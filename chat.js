a5a9ddeb7d6456366551de34f10bcf488f972149
Volcos <volcothevolcanion@gmail.com>
2016-12-16 21:40:13 -0500

    Emoticons: Re-add deleted line (#25)
    I accidentally deleted a line that i didnt remove in testing

--- chat.js
+++ chat.js
@@ -202,6 +202,7 @@ class CommandContext {
 					}
 					this.room.log.push((this.room.type === 'chat' ? (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
 					this.room.lastUpdate = this.room.log.length;
+					this.room.messageCount++;
 				} else {
 					if (Users.ShadowBan.checkBanned(this.user)) {
 						Users.ShadowBan.addMessage(this.user, "To " + this.room.id, message);
