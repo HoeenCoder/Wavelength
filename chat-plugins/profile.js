528591e4b7c3e459f2a3a9940c9ba3f644a317e8
HoeenHero <HoeenCoder@users.noreply.github.com>
2016-12-18 17:20:10 -0500

    Remove leagues.js
    This is probably going to be temporary, as the type of league system we
    would use in the future would have to be very different so im removing
    this to allow a quick depreciation of SQlite3

--- chat-plugins/profile.js
+++ chat-plugins/profile.js
@@ -65,7 +65,8 @@ function showBadges(user) {
 }
 
 function getLeague(userid) {
-	return SG.getLeague(userid);
+	return false; //TEMPORARY
+	//return SG.getLeague(userid);
 }
 
 /*function loadRegdateCache() {
@@ -303,7 +304,7 @@ exports.commands = {
 				profile += '&nbsp;<font color="#24678d"><b>Group:</b></font> ' + userGroup + ' ' + devCheck(username) + vipCheck(username) + ' ' + showBadges(toId(username)) + '<br />';
 				profile += '&nbsp;<font color="#24678d"><b>Registered:</b></font> ' + regdate + '<br />';
 				profile += '&nbsp;<font color="#24678d"><b>' + global.currencyPlural + ':</b></font> ' + currency + '<br />';
-				profile += '&nbsp;<font color="#24678d"><b>League:</b></font> ' + (getLeague(toId(username)) ? (getLeague(toId(username)) + ' (' + SG.getLeagueRank(toId(username)) + ')') : 'N/A') + '<br />';
+				profile += '&nbsp;<font color="#24678d"><b>League:</b></font> ' + (getLeague(toId(username)) ? (getLeague(toId(username)) + ' (' + getLeagueRank(toId(username)) + ')') : 'N/A') + '<br />';
 				if (getLastSeen(toId(username))) {
 					profile += '&nbsp;<font color="#24678d"><b>Last Seen:</b></font> ' + getLastSeen(toId(username)) + '</font><br />';
 				} else {
