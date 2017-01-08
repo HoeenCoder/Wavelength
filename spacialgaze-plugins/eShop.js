a7af2abd4b7d60cc54877b3d62fe7d884f20bdbc
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-01-08 08:00:32 -0500

    Prepare for shop tokens

--- spacialgaze-plugins/eShop.js
+++ spacialgaze-plugins/eShop.js
@@ -155,6 +155,8 @@ exports.commands = {
 							writeSSB();
 							break;
 						default:
+							if (!user.tokens) user.tokens = {};
+							user.tokens[item.id] = true;
 							SG.messageSeniorStaff(user.name + ' has purchased a ' + item.name + '.');
 						}
 						user.sendTo(room, "|uhtmlchange|eshop" + user.userid + "|<div style='max-height:300px'><table style='border:2px solid #101ad1; border-radius: 5px'><tr><th colspan='3' style='border: 2px solid #070e96; border-radius: 5px'>Server Shop</th></tr><tr><td style='colspan: 3; border: 2px solid #070e96; border-radius: 5px'><center>You have purchased a " + item.name + ". " + (item.id === 'customsymbol' ? "You may now use /customsymbol [symbol] to change your symbol." : "Upper staff have been notified of your purchase and will contact you shortly.") + "</center></td></tr><tr><td colspan='3' style='text-align:center'><button name='send' value='/eshop reopen'>Return to Shop</button></td></tr></table>");
