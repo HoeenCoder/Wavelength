fa2a17c3297c7d0059e7f25721342c1ced9db5d8
Lord-Haji <lordhajisinbox@gmail.com>
2017-01-17 19:17:43 +0530

    Introducing SpacialGaze News System (#50)

--- users.js
+++ users.js
@@ -656,6 +656,7 @@ class User {
 		}
 
 		if (Tells.inbox[userid]) Tells.sendTell(userid, this);
+		SG.showNews(userid, this);
 		return false;
 	}
 	validateRename(name, tokenData, newlyRegistered, challenge) {
