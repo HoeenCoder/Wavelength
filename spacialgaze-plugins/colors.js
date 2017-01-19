cc0c7d1fa5e9001ced73ad9bc7fe3b6362a00f18
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-01-18 19:24:19 -0500

    Color fixes

--- spacialgaze-plugins/colors.js
+++ spacialgaze-plugins/colors.js
@@ -6,40 +6,66 @@
  */
 'use strict';
 
-let fs = require('fs');
-let http = require('http');
+const fs = require('fs');
+const http = require('http');
 
 let customColors = {};
-const FILE_PATH = 'config/customcolors.json';
+let cssPath = 'spacialgaze'; // This should be the server id if Config.serverid doesn't exist. Ex: 'serverid'
+
+
+function reloadCSS() {
+		let options = {
+			host: 'play.pokemonshowdown.com',
+			port: change it,
+			path: '/customcss.php?server=' + (Config.serverid || cssPath),
+			method: 'GET',
+		};
+		http.get(options);
+}
 
 function load() {
-	fs.readFile(FILE_PATH, 'utf8', (err, file) => {
+	fs.readFile('config/customcolors.json', 'utf8', function (err, file) {
 		if (err) return;
 		customColors = JSON.parse(file);
 	});
 }
-load();
+setInterval(function () {
+	load();
+}, 500);
 
-/*function logMoney(message) {
-       if (!message) return;
-       var file = path.join(__dirname, '../logs/money.txt');
-       var date = "[" + new Date().toUTCString() + "] ";
-       var msg = message + "\n";
-       fs.appendFile(file, date + msg);
-}*/
+function updateColor() {
+	fs.writeFileSync('config/customcolors.json', JSON.stringify(customColors));
 
-let cssPath = 'spacialgaze'; // This should be the server id if Config.serverid doesn't exist. Ex: 'serverid'
+	let newCss = '/* COLORS START */\n';
 
-function getCSS() {
-	let options = {
-		host: 'play.pokemonshowdown.com',
-		port: 80,
-		path: '/customcss.php?server=' + (Config.serverid || cssPath),
-		method: 'GET',
-	};
-	http.get(options);
+	for (let name in customColors) {
+		newCss += generateCSS(name, customColors[name]);
+	}
+	newCss += '/* COLORS END */\n';
+
+	let file = fs.readFileSync('config/custom.css', 'utf8').split('\n');
+	if (~file.indexOf('/* COLORS START */')) file.splice(file.indexOf('/* COLORS START */'), (file.indexOf('/* COLORS END */') - file.indexOf('/* COLORS START */')) + 1);
+	fs.writeFileSync('config/custom.css', file.join('\n') + newCss);
+	reloadCSS();
 }
-SG.loadCSS = getCSS;
+
+function generateCSS(name, color) {
+	let css = '';
+	let rooms = [];
+	name = toId(name);
+	Rooms.rooms.forEach((curRoom, id) => {
+		if (id === 'global' || curRoom.type !== 'chat' || curRoom.isPersonal) return;
+		if (!isNaN(Number(id.charAt(0)))) return;
+		rooms.push('#' + id + '-userlist-user-' + name + ' strong em');
+		rooms.push('#' + id + '-userlist-user-' + name + ' strong');
+		rooms.push('#' + id + '-userlist-user-' + name + ' span');
+	});
+	css = rooms.join(', ');
+	css += '{\ncolor: ' + color + ' !important;\n}\n';
+	css += '.chat.chatmessage-' + name + ' strong {\n';
+	css += 'color: ' + color + ' !important;\n}\n';
+	return css;
+}
 
 function updateColor() {
 	fs.writeFileSync(FILE_PATH, JSON.stringify(customColors));
@@ -73,41 +99,54 @@ function generateCSS(name, color) {
 }
 
 exports.commands = {
-	customcolor: function (target, room, user) {
-		if (!this.can('forcewin')) return false;
-		target = target.split(',').map(param => param.trim());
-		if (target.length !== 2) return this.parse('/help customcolor');
-		let targetUser = toId(target[0]);
-		let option = target[1];
-		if (targetUser.length > 18) return this.errorReply("Usernames cannot exceed 18 characters.");
-		if (option === 'delete') {
-			if (!customColors[targetUser]) return this.errorReply(`/customcolor - ${target[0]} does not have a custom color.`);
-			delete customColors[targetUser];
+	customcolour: 'customcolor',
+	customcolor: {
+		set: function (target, room, user) {
+			if (!this.can('roomowner')) return false;
+			target = target.split(',');
+			for (let u = 0; u < target.length; u++) target[u] = target[u].trim();
+			if (!target[1]) return this.parse('/help customcolor');
+			if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
+
+			this.sendReply("|raw|You have given <b><font color=" + target[1] + ">" + Chat.escapeHTML(target[0]) + "</font></b> a custom color.");
+			this.privateModCommand("(" + target[0] + " has recieved custom color: '" + target[1] + "' from " + user.name + ".)");
+			customColors[toId(target[0])] = target[1];
 			updateColor();
-			this.sendReply(`You removed ${target[0]}'s custom color.`);
-			if (Users(targetUser) && Users(targetUser).connected) Users(targetUser).popup(`${user.name} removed your custom color.`);
+		},
+		delete: function (target, room, user) {
+			if (!this.can('roomowner')) return false;
+			if (!target) return this.parse('/help customcolor');
+			if (!customColors[toId(target)]) return this.errorReply('/customcolor - ' + target + ' does not have a custom color.');
+			delete customColors[toId(target)];
+			updateColor();
+			this.sendReply("You removed " + target + "'s custom color.");
+			this.privateModCommand("(" + target + "'s custom color was removed by " + user.name + ".)");
+			if (Users(target) && Users(target).connected) Users(target).popup(user.name + " removed your custom color.");
 			return;
-		}
-
-		if (option.charAt(0) !== '#') return this.errorReply("The color needs to be a hex starting with '#'.");
-		this.sendReply(`|raw|You have given <b><font color="${option}">${Chat.escapeHTML(target[0])}</font></b> a custom color.`);
-		// logMoney(user.name + " assigned a custom color to " + target[0] + ". (Color: " + target[1] + ").");
-		customColors[targetUser] = option;
-		updateColor();
+		},
+		preview: function (target, room, user) {
+			if (!this.runBroadcast()) return;
+			target = target.split(',');
+			for (let u = 0; u < target.length; u++) target[u] = target[u].trim();
+			if (!target[1]) return this.parse('/help customcolor');
+			return this.sendReplyBox('<b><font size="3" color="' + target[1] + '">' + Chat.escapeHTML(target[0]) + '</font></b>');
+		},
+		reload: function (target, room, user) {
+			if (!this.can('hotpatch')) return false;
+			updateColor();
+			this.privateModCommand("(" + user.name + " has reloaded custom colours.)");
+		},
+		'': function (target, room, user) {
+			return this.parse("/help customcolor");
+		},
 	},
-	customcolorhelp: ["Commands Include:",
-		"/customcolor [user], [hex] - Gives [user] a custom color of [hex]",
-		"/customcolor [user], delete - Deletes a user's custom color",
+	customcolorhelp: [
+		"Commands Include:",
+		"/customcolor set [user], [hex] - Gives [user] a custom color of [hex]",
+		"/customcolor delete [user], delete - Deletes a user's custom color",
+		"/customcolor reload - Reloads colours.",
+		"/customcolor preview [user], [hex] - Previews what that username looks like with [hex] as the color.",
 	],
-
-	cp: 'colorpreview',
-	colorpreview: function (target, room, user) {
-		if (!this.canBroadcast()) return;
-		target = target.split(',').map(param => param.trim());
-		if (target.length !== 2) return this.parse('/help colorpreview');
-		return this.sendReplyBox(`<b><font size="2" color="${target[1]}">${Chat.escapeHTML(target[0])}</font></b>`);
-	},
-	colorpreviewhelp: ["Usage: /colorpreview [user], [color] - Previews what that username looks like with [color] as the color."],
 };
 
 /* Pokemon Showdown hashColor function
