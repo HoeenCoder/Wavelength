18a7388f35c791d1df6035fd66e37d00ba3b689f
Executive Dronpes <lordhajisinbox@gmail.com>
2017-02-09 07:23:34 +0530

    Add usergroup symbol display at nameColor (#96)

--- spacialgaze-plugins/SG.js
+++ spacialgaze-plugins/SG.js
@@ -6,8 +6,9 @@ const Autolinker = require('autolinker');
 
 let regdateCache = {};
 
-SG.nameColor = function (name, bold) {
-	return (bold ? "<b>" : "") + "<font color=" + SG.hashColor(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Chat.escapeHTML(Users.getExact(name).name) : Chat.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
+SG.nameColor = function (name, bold, userGroup) {
+	let userGroupSymbol = Users.usergroups[name] ? '<b><font color=#948A88>' + Users.usergroups[name].substr(0, 1) + '</font></b>' : "";
+	return (userGroup ? userGroupSymbol : "") + (bold ? "<b>" : "") + "<font color=" + SG.hashColor(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Chat.escapeHTML(Users.getExact(name).name) : Chat.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
 };
 // usage: SG.nameColor(user.name, true) for bold OR SG.nameColor(user.name, false) for non-bolded.
 
