4ce7c380214a748aeaf1d63d3d6fddc876fe8bcc
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-02-10 12:10:23 -0500

    Crash fix

--- spacialgaze-plugins/profile.js
+++ spacialgaze-plugins/profile.js
@@ -307,7 +307,7 @@ exports.commands = {
 		}
 
 		function getFlag(userid) {
-			let ip = geoip.lookup(Users(userid).latestIp);
+			let ip = (Users(userid) ? geoip.lookup(Users(userid).latestIp) : false);
 			if (!ip || ip === null) return '';
 			return '<img src="http://flags.fmcdn.net/data/flags/normal/' + ip.country.toLowerCase() + '.png" alt="' + ip.country + '" title="' + ip.country + '" width="20" height="10">';
 		}
