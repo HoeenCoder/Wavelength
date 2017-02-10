9caa475ab6a89e58e7cbdf9d23a1be32e7b2aaa7
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-02-10 14:51:44 -0500

    Leaders can blockpms

--- chat.js
+++ chat.js
@@ -607,9 +607,9 @@ class CommandContext {
 				}
 				if (targetUser.ignorePMs && targetUser.ignorePMs !== user.group && !user.can('lock')) {
 					if (!targetUser.can('lock')) {
-						return this.errorReply(`This user is blocking private messages right now.`);
-					} else if (targetUser.can('bypassall')) {
-						return this.errorReply(`This admin is too busy to answer private messages right now. Please contact a different staff member.`);
+						return this.errorReply(`This user is blocking private messages right now.`);	   
+					} else if (targetUser.can('roomowner')) {
+						return this.errorReply(`This ` + (targetUser.can('bypassall') ? `admin` : `leader`) + ` is too busy to answer private messages right now. Please contact a different staff member.`);
 					}
 				}
 				if (user.ignorePMs && user.ignorePMs !== targetUser.group && !targetUser.can('lock')) {
