c7a97ac3f409bc9d7fe5fd38a1f84854cbad701f
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-02-18 19:16:16 -0500

    Rocket Punch is a Fire type move

--- mods/sgssb/moves.js
+++ mods/sgssb/moves.js
@@ -232,7 +232,7 @@ exports.BattleMovedex = {
 		pp: 10,
 		priority: 1,
 		target: "Normal",
-		type: "Fighting",
+		type: "Fire",
 		onPrepareHit: function (target, source) {
 			this.attrLastMove('[still]');
 			this.add('-anim', source, "Flare Blitz", source);
