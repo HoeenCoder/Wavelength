9264e8d6823127da131c16e4d76e46a8b59ec08c
Executive Dronpes <lordhajisinbox@gmail.com>
2017-03-03 18:13:09 +0530

    Use userid when getting a users group (#130)

--- spacialgaze-plugins/SG.js
+++ spacialgaze-plugins/SG.js
@@ -7,7 +7,7 @@ const Autolinker = require('autolinker');
 let regdateCache = {};
 
 SG.nameColor = function (name, bold, userGroup) {
-	let userGroupSymbol = Users.usergroups[toId(name)] ? '<b><font color=#948A88>' + Users.usergroups[name].substr(0, 1) + '</font></b>' : "";
+	let userGroupSymbol = Users.usergroups[toId(name)] ? '<b><font color=#948A88>' + Users.usergroups[toId(name)].substr(0, 1) + '</font></b>' : "";
 	return (userGroup ? userGroupSymbol : "") + (bold ? "<b>" : "") + "<font color=" + SG.hashColor(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Chat.escapeHTML(Users.getExact(name).name) : Chat.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
 };
 // usage: SG.nameColor(user.name, true) for bold OR SG.nameColor(user.name, false) for non-bolded.
