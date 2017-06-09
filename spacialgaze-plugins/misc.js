17d918c6e998180c3b04e167b652a88d6e5ff834
Insist <DeathlyPlays@users.noreply.github.com>
2017-06-09 11:08:23 -0500

    Credits: Remove AeonLucid
    After recent actions, we no longer can affiliate with AeonLucid on SpacialGaze.

--- spacialgaze-plugins/misc.js
+++ spacialgaze-plugins/misc.js
@@ -150,7 +150,6 @@ exports.commands = {
 			"- " + SG.nameColor('Opple', true) + " (Policy and Media)<br />" +
 			"- " + SG.nameColor('Kraken Mare', true) + " (Development)<br />" +
 			"- " + SG.nameColor('HiroZ', true) + " (Policy)<br />" +
-			"- " + SG.nameColor('AeonLucid', true) + " (Development)<br />" +
 			"- " + SG.nameColor('Ashley the Pikachu', true) + " (CSS, Spriting)<br />" +
 			"- " + SG.nameColor('Insist', true) + " (Development)<br />" +
 			"- " + SG.nameColor('VXN', true) + " (Development)<br />" +
