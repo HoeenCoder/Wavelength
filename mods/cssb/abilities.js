a1dbf098e3c79c5bdb0348c65dc10acef82ea9d3
Insist <DeathlyPlays@users.noreply.github.com>
2017-08-22 17:09:38 -0500

    SSBFFA: Add Insist's Ability

--- mods/cssb/abilities.js
+++ mods/cssb/abilities.js
@@ -98,4 +98,48 @@ exports.BattleAbilities = {
 		},
 		desc: "Permanent Electric Terrain, unless user switches out. Speed is doubled.",
 	},
+	//Insist
+	cripplingdepression: {
+		id: "cripplingdepression",
+		name: "Crippling Depression",
+		isNonStandard: true,
+		desc: "Primordial Sea + Rain Dish + Swift Swim",
+		//primordialseas
+		onStart: function (source) {
+			this.setWeather('primordialsea');
+		},
+		onAnySetWeather: function (target, source, weather) {
+			if (this.getWeather().id === 'primordialsea' && !(weather.id in {
+				desolateland: 1,
+				primordialsea: 1,
+				deltastream: 1,
+			})) return false;
+		},
+		onEnd: function (pokemon) {
+			if (this.weatherData.source !== pokemon) return;
+			for (let i = 0; i < this.sides.length; i++) {
+				for (let j = 0; j < this.sides[i].active.length; j++) {
+					let target = this.sides[i].active[j];
+					if (target === pokemon) continue;
+					if (target && target.hp && target.hasAbility('primordialsea')) {
+						this.weatherData.source = target;
+						return;
+					}
+				}
+			}
+			this.clearWeather();
+		},
+		//raindish
+		onWeather: function (target, source, effect) {
+			if (effect.id === 'raindance' || effect.id === 'primordialsea') {
+				this.heal(target.maxhp / 16);
+			}
+		},
+		//swiftswim
+		onModifySpe: function (spe, pokemon) {
+			if (this.isWeather(['raindance', 'primordialsea'])) {
+				return this.chainModify(2);
+			}
+		},
+	},
 };
