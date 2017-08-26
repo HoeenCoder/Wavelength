38820ffc6262165dec59246fb17bd00d6a72c91f
returningavenger <miguelmayers2001@gmail.com>
2017-08-26 00:09:29 +0000

    Uno: Fix HTML Bust

--- chat-plugins/uno.js
+++ chat-plugins/uno.js
@@ -486,7 +486,7 @@ class UNOgame extends Rooms.RoomGame {
 	onUno(user, unoId) {
 		// uno id makes spamming /uno uno impossible
 		if (this.unoId !== unoId || user.userid !== this.awaitUno) return false;
-		this.sendToRoom(Chat.html`|raw|<strong>UNO!</strong> ${SG.nameColor(user.name, true, true)} is down to their last card!`);
+		this.sendToRoom(Chat.html`|raw|<strong>UNO!</strong> ${user.name} is down to their last card!`);
 		delete this.awaitUno;
 		delete this.unoId;
 	}
