19c783d442a44b4334d6fec8f61a3f7026e11651
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-11-04 18:47:15 -0400

    Update lottery.js

--- wavelength-plugins/lottery.js
+++ wavelength-plugins/lottery.js
@@ -13,7 +13,7 @@ class Lottery {
 		room.lottoNumber = room.lottoNumber ? room.lottoNumber++ : 1;
 		this.lottoNumber = room.lottoNumber;
 		this.costToJoin = 3;
-		this.room.add(`|uhtml|lottery-${this.lottoNumber}|<div class="broadcast-blue"><p style="font-size: 14pt; text-align: center">A new <strong>Lottery drawing</strong> is starting!</p><p style="font-size: 9pt; text-align: center"><button name="send" value="/lotto join">Join</button><br /><strong>DISCLAIMER: Joining costs ${this.costToJoin} ${currencyPlural}!!!!</strong></p></div>`, true);
+		this.room.add(`|uhtml|lottery-${this.lottoNumber}|<div class="broadcast-blue"><p style="font-size: 14pt; text-align: center">A new <strong>Lottery drawing</strong> is starting!</p><p style="font-size: 9pt; text-align: center"><button name="send" value="/lotto join">Join</button><br /><strong>Joining costs ${this.costToJoin} ${currencyPlural}</strong></p></div>`, true);
 		this.timer = setTimeout(() => {
 			if (this.players.length < 2) {
 				this.room.add('|uhtmlchange|lottery-' + this.lottoNumber + '|<div class="broadcast-red"><p style="text-align: center; font-size: 14pt>This Lottery drawing has ended due to lack of users.</p></div>');
@@ -92,6 +92,10 @@ exports.commands = {
 		leave: function (target, room, user) {
 			if (!room.lottery) return this.sendReply("There is no active Lottery drawing in this room.");
 			room.lottery.leaveLottery(user);
+		},
+		players: function (target, room, user) {
+			if (!room.lottery) return this.sendReply("There is no active Lottery drawing in this room.");
+			return this.sendReply('There are ' + room.lottery.players.length + ' users in the lottery.');
 		},
 		forcestart: "start",
 		begin: "start",
