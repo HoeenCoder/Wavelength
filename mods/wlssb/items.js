ccb84b4c3969ed58108296ad32c6203fa33d8def
Lycanium-Z <28816776+Lycanium-Z@users.noreply.github.com>
2017-11-04 20:43:55 -0400

    Add mine pt 1

--- mods/wlssb/items.js
+++ mods/wlssb/items.js
@@ -1,15 +1,5 @@
 'use strict';
 
 exports.BattleItems = {
-	notthelycaniumziswear: {
-		spritenum: 656,
-		onTakeItem: false,
-		id: "notthelycaniumziswear",
-		name: "Not the Lycanium Z i swear",
-		isNonStandard: true,
-		zMove: "Wreak Havoc",
-		zMoveFrom: "Alt Storm",
-		zMoveUser: ["Lycanroc"],
-		desc: "If holder is a Lycanroc with Alt Storm, it can use Wreak Havoc.",
-	},
+//Empty
 };
