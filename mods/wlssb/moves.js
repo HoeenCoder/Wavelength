6a8c9d9a7dcf7cc35b7fbaf3928da1230c04e809
Lycanium-Z <28816776+Lycanium-Z@users.noreply.github.com>
2017-11-06 08:50:48 -0500

    Update moves.js

--- mods/wlssb/moves.js
+++ mods/wlssb/moves.js
@@ -379,7 +379,7 @@ exports.BattleMovedex = {
 			chance: 100,
 			volatileStatus: 'confusion',
 		},
-		recoil: [9,10],
+		recoil: [9, 10],
 		target: "normal",
 		type: "Normal",
 		zMovePower: 100,
