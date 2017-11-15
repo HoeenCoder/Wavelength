3845cf120cab993f4b906ec1736fee9daf21a274
Lycanium-Z <28816776+Lycanium-Z@users.noreply.github.com>
2017-11-15 18:42:06 -0500

    Travis

--- mods/oldweather/moves.js
+++ mods/oldweather/moves.js
@@ -1,48 +1,48 @@
 'use strict';
 
 exports.BattleMovedex = {
-    "grassyterrain": {
-        inherit: true,
-        onStart: function (battle, source, effect) {
+	"grassyterrain": {
+		inherit: true,
+		onStart: function (battle, source, effect) {
 			if (effect && effect.effectType === 'Ability') {
-			    this.effectData.duration = 0;
-		    	this.add('-fieldstart', 'move: Grassy Terrain', '[from] ability: ' + effect, '[of] ' + source);
-		   	} else {
+				this.effectData.duration = 0;
+				this.add('-fieldstart', 'move: Grassy Terrain', '[from] ability: ' + effect, '[of] ' + source);
+			} else {
 				this.add('-fieldstart', 'move: Grassy Terrain');
 			}
 		},
-    },
-    "electricterrain": {
-        inherit: true,
-        onStart: function (battle, source, effect) {
+	},
+	"electricterrain": {
+		inherit: true,
+		onStart: function (battle, source, effect) {
 			if (effect && effect.effectType === 'Ability') {
-			    this.effectData.duration = 0;
+				this.effectData.duration = 0;
 				this.add('-fieldstart', 'move: Electric Terrain', '[from] ability: ' + effect, '[of] ' + source);
-			} else {   
-			    this.add('-fieldstart', 'move: Electric Terrain');
+			} else {
+				this.add('-fieldstart', 'move: Electric Terrain');
 			}
 		},
-    },
-    "psychicterrain": {
-        inherit: true,
-        onStart: function (battle, source, effect) {
+	},
+	"psychicterrain": {
+		inherit: true,
+		onStart: function (battle, source, effect) {
 			if (effect && effect.effectType === 'Ability') {
-			    this.effectData.duration = 0;
+				this.effectData.duration = 0;
 				this.add('-fieldstart', 'move: Psychic Terrain', '[from] ability: ' + effect, '[of] ' + source);
 			} else {
-			    this.add('-fieldstart', 'move: Psychic Terrain');
+				this.add('-fieldstart', 'move: Psychic Terrain');
 			}
 		},
-    },
-    "mistyterrain": {
-        inherit: true,
-        onStart: function (battle, source, effect) {
+	},
+	"mistyterrain": {
+		inherit: true,
+		onStart: function (battle, source, effect) {
 			if (effect && effect.effectType === 'Ability') {
-			    this.effectData.duration = 0;
+				this.effectData.duration = 0;
 				this.add('-fieldstart', 'move: Misty Terrain', '[from] ability: ' + effect, '[of] ' + source);
 			} else {
 				this.add('-fieldstart', 'move: Misty Terrain');
 			}
 		},
-    },
+	},
 };
