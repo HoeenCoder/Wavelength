d305f92de2679ff6b72db53a44ad0029ac22f6eb
Lycanium-Z <28816776+Lycanium-Z@users.noreply.github.com>
2017-11-21 23:07:39 -0500

    oops

--- mods/wlssb/random-teams.js
+++ mods/wlssb/random-teams.js
@@ -40,7 +40,7 @@ class RandomSeasonalRegStaffTeams extends RandomTeams {
 				},
 				nature: 'Bold',
 			},
-			'~~Kraken Mare': {
+			'~Kraken Mare': {
 				species: 'Malamar',
 				ability: 'Supreme Squid Sister',
 				item: 'Hypnoshades',
