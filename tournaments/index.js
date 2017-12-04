82c38c59c878dbc660ed794f1d85d7266852928c
Lycanium-Z <28816776+Lycanium-Z@users.noreply.github.com>
2017-12-03 23:22:49 -0500

    Tours: Fix detrimental bug
    Makes sure players are destroyed

--- tournaments/index.js
+++ tournaments/index.js
@@ -996,13 +996,13 @@ class Tournament {
 
 		delete exports.tournaments[this.room.id];
 		delete this.room.game;
-		if (this.room.isOfficial) {
-			for (let i in this.players) {
-				Users(this.players[i].userid).tourBoost = false;
-				Users(this.players[i].userid).gameBoost = false;
+		for (let i in this.players) {
+			Users(this.players[i].userid).tourBoost = false;
+			Users(this.players[i].userid).gameBoost = false;
+			if (this.room.isOfficial) {
 				WL.addExp(this.players[i].userid, this.room, 20);
-				this.players[i].destroy();
 			}
+			this.players[i].destroy();
 		}
 	}
 }
