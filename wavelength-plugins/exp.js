e5411536bb5eb12ee848bb76c4311e78be27f29f
Arrays <31297626+Arrays-Arr@users.noreply.github.com>
2017-12-10 10:10:20 -0500

    Exp bans: Add modlogging

--- wavelength-plugins/exp.js
+++ wavelength-plugins/exp.js
@@ -214,6 +214,8 @@ exports.commands = {
 		let targetId = toId(target);
 		if (!Db.expoff.has(targetId)) return this.errorReply(targetId + ' is not currently exp banned.');
 		Db.expoff.remove(targetId);
+		this.globalModlog("EXPUNBAN", targetId, " by " + user.name);
+		this.addModCommand(targetId + ' was exp unbanned by ' + user.name + '.');
 		this.sendReply(targetId + " is no longer banned from exp");
 	},
 	expunbanhelp: ['/expunban target - allows a user to gain exp if they were exp banned'],
@@ -224,6 +226,8 @@ exports.commands = {
 		let targetId = toId(target);
 		if (Db.expoff.has(targetId)) return this.errorReply(targetId + ' is already exp banned.');
 		Db.expoff.set(targetId, true);
+		this.globalModlog("EXPBAN", targetId, " by " + user.name);
+		this.addModCommand(targetId + ' was exp banned by ' + user.name + '.');
 		this.sendReply(targetId + " is now banned from exp");
 	},
 	expbanhelp: ['/expban target - bans a user from gaining exp until removed'],
