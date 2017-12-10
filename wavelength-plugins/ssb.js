fd0e4510ace42e4a6930b5d250574bf6daa27acc
Arrays <31297626+Arrays-Arr@users.noreply.github.com>
2017-12-10 11:14:13 -0500

    add buying check

--- wavelength-plugins/ssb.js
+++ wavelength-plugins/ssb.js
@@ -1014,6 +1014,7 @@ exports.commands = {
 			let customMove = toId(targets[1]);
 			if (!customMove) return this.errorReply('Must include a move!');
 			if (!Dex.mod('cssb').getMove(customMove).exists) return this.errorReply("Move doesn't exist in the ssbffa mod!");
+			if (!WL.ssb[userid].bought.cMove) return this.errorReply('They have not bought a custom move!');
 			WL.ssb[userid].selfCustomMove = customMove;
 			writeSSB();
 			return this.sendReply('Move set for ' + userid + '!');
@@ -1029,6 +1030,7 @@ exports.commands = {
 			let customAbility = toId(targets[1]);
 			if (!customAbility) return this.errorReply('/ssb giveability target, ability');
 			if (!Dex.mod('cssb').getAbility(customAbility).exists) return this.errorReply("Ability doesn't exist in the ssbffa mod!");
+			if (!WL.ssb[userid].bought.cAbility) return this.errorReply('They have not bought a custom ability!');
 			WL.ssb[userid].cAbility = customAbility;
 			writeSSB();
 			return this.sendReply('Ability set for ' + userid + '!');
@@ -1044,6 +1046,7 @@ exports.commands = {
 			let item = toId(targets[1]);
 			if (!item) return this.errorReply('Must include an item');
 			if (!Dex.mod('cssb').getItem(item).exists) return this.errorReply("Item doesn't exist in the ssbffa mod!");
+			if (!WL.ssb[userid].bought.cItem) return this.errorReply('They have not bought a custom item!');
 			WL.ssb[userid].cItem = item;
 			writeSSB();
 			return this.sendReply('Item set for ' + userid + '!');
