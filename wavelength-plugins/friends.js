820634b0b87cb4723081cbc6fc0c24c7407f453f
Austin <ajrussell200058@yahoo.com>
2017-12-14 17:40:55 +0000

    r u srs

--- wavelength-plugins/friends.js
+++ wavelength-plugins/friends.js
@@ -14,20 +14,20 @@ function genFC() {
 	return uniqueCode;
 }
 const FC = {
-	getFC: function(user) {
+	getFC: function (user) {
 		user = toId(user);
 		let fcget = Db.fc.get(user);
 		if (!fcget || fcget === null) return user.reply('You don\'t have a friendcode. Use /fc make to make one.');
 		return fcget;
 	},
-	addFC: function(user, fc) {
+	addFC: function (user, fc) {
 		user = toId(user);
 		Db.fc.set(user, fc);
 		return user.reply('Your friend code has been added to the system. Give to others to add you as a friend!');
 	},
 };
 const Friend = {
-	addFriend: function(target, fc, user) {
+	addFriend: function (target, fc, user) {
 		target = toId(target);
 		fc = fc.toLowerCase();
 		let thisFC = FC.getFC(target).toLowerCase();
@@ -42,7 +42,7 @@ const Friend = {
 			return;
 		}
 	},
-	removeFriend: function(target, user) {
+	removeFriend: function (target, user) {
 		target = toId(target);
 		if (!Db.friends.get(user).includes(target)) return this.errorReply('You are not friends with this user.');
 		let userid = user.userid;
@@ -58,7 +58,7 @@ const Friend = {
 };
 exports.commands = {
 	fc: {
-		make: function(target, room, user) {
+		make: function (target, room, user) {
 			let hasFC = Db.fc.get(user);
 			if (!hasFC) {
 				if (user.locked) this.errorReply('You can\'t obtain your friend code while locked.');
@@ -69,18 +69,18 @@ exports.commands = {
 				this.popupReply('You already have a friend code.');
 			}
 		},
-		get: function(target, room, user) {
+		get: function (target, room, user) {
 			if (user.locked) this.errorReply('You can\'t give out your code while locked.');
 			let getfc = FC.getFC(user);
 			this.sendReplyBox(`Your friend code is: ${getfc}`);
 		},
-		help: function(target, room, user) {
+		help: function (target, room, user) {
 			this.sendReplyBox('<h2> Friend Code Commands: </h2><br />' + '<i> /fc make </i> - Makes your friend code. <br />' + '<i> /fc get </i> - Displays your friend code (in case you forget.)');
 		},
 	},
 	friends: 'friend',
 	friend: {
-		add: function(target, room, user) {
+		add: function (target, room, user) {
 			if (user.locked) this.errorReply('You can\'t make friends while locked.');
 			let [targetUser, targetCode] = target.split(',').map(p => toId(p));
 			if (!targetUser || !targetCode) return this.errorReply('/friend add [friend], [fc]');
@@ -89,18 +89,18 @@ exports.commands = {
 			if (targetUser > 19) return this.errorReply('Invalid user.');
 			Friend.addFriend(targetUser, targetCode, user);
 		},
-		remove: function(target, room, user) {
+		remove: function (target, room, user) {
 			if (!target) return this.errorReply('/friend remove [friend]');
 			if (target < 1) return this.errorReply('Invalid user.');
 			if (target > 19) return this.errorReply('Invalid user.');
 			Friend.removeFriend(target, user);
 		},
-		removeall: function(target, room, user) {
+		removeall: function (target, room, user) {
 			if (Db.friends.get(user).length === 0) this.errorReply('You have no friends.');
 			Db.friends.set(Db.friends.get(user), []);
 			this.sendReply('You have removed all of your friends');
 		},
-		list: function(target, room, user) {
+		list: function (target, room, user) {
 			let array = Db.friends.get(user, []);
 			let amount = array.length;
 			if (!this.runBroadcast()) return;
@@ -110,7 +110,7 @@ exports.commands = {
 				return this.sendReply(`You have ${amount} friend${Chat.plural(amount)}: ${array.join(', ')}`);
 			}
 		},
-		help: function(target, room, user) {
+		help: function (target, room, user) {
 			this.sendReplyBox('<h2> Friends Commands: </h2> <br />' + '<i> /friend add [target], [friend code] </i> - Adds a friend. <br />' + '<i> /friend remove [target] </i> - Removes a friend. <br />' + '<i> /friend list </i> - Lists all your friends. <br />' + '<i> /friend removeall </i> - Removes all friends.');
 		},
 	},
