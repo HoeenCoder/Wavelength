51827d59722e2119beca9f300635b669b3e87dc6
Volco <13051425+Volco@users.noreply.github.com>
2018-01-03 09:31:46 -0500

    Update profile.js

--- wavelength-plugins/profile.js
+++ wavelength-plugins/profile.js
@@ -523,7 +523,7 @@ exports.commands = {
 				}
 				profile += '&nbsp;<font color="#24678d"><b>Last Seen:</b></font> ' + getLastSeen(toId(username)) + '</font><br />';
 				if (Db.friendcodes.has(toId(username))) {
-					profile += '&nbsp;<font color="#24678d"><b>Friend Code:</b></font> ' + Db.friendcodes.get(toId(username));
+					profile += '&nbsp;<font color="#24678d"><b>Friend Code:</b></font> ' + Db.friendcodes.get(toId(username) + '<br />');
 				}
 				profile += '&nbsp;' + song(toId(username)) + '';
 				profile += '&nbsp;</div>';
