5706edabca960cdeb563244f17cfd0f66dcb9c63
Lycanium-Z <28816776+Lycanium-Z@users.noreply.github.com>
2018-01-13 20:45:05 -0500

    Update pokedex.js

--- mods/wlssb/pokedex.js
+++ mods/wlssb/pokedex.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports.BattlePokedex = {
-    arceusdark: {
-        inherit: true,
-        baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
-    },
+	arceusdark: {
+		inherit: true,
+		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
+	},
 };
