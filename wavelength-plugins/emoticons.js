bb5afaed33572c81d41cad17d1c4291073b51166
Volco <13051425+Volco@users.noreply.github.com>
2018-01-15 15:12:06 -0500

    Update emoticons.js

--- wavelength-plugins/emoticons.js
+++ wavelength-plugins/emoticons.js
@@ -73,7 +73,7 @@ exports.commands = {
 			if (emoticons[parts[0]]) return this.sendReply(`"${parts[0]}" is already an emoticon.`);
 			emoticons[parts[0]] = parts[1];
 			saveEmoticons();
-			this.sendReply(`|raw|The emoticon "${Chat.escapeHTML(parts[0])}" has been added: <img src="${parts[1]}">`);
+			this.sendReply(`|raw|The emoticon "${Chat.escapeHTML(parts[0])}" has been added: <img src="${parts[1]}" width="40" height="40">`);
 			Rooms('upperstaff').add(`|raw|${WL.nameColor(user.name, true)} has added the emote "${Chat.escapeHTML(parts[0])}": <img width="40" height="40" src="${parts[1]}">`).update();
 			WL.messageSeniorStaff(`/html ${WL.nameColor(user.name, true)} has added the emote "${Chat.escapeHTML(parts[0])}": <img width="40" height="40" src="${parts[1]}">`);
 		},
