b3505111b0874db7b7f8a6c30a691afb9e71f845
Volco <13051425+Volco@users.noreply.github.com>
2018-01-17 08:16:44 -0500

    Update exp.js

--- wavelength-plugins/exp.js
+++ wavelength-plugins/exp.js
@@ -71,7 +71,7 @@ class ExpFunctions {
 	
 	nextLevel(user) {
 		let curExp = Db.exp.get(toId(user), 0);
-		let lvl = WL.level(toId(user));
+		let lvl = this.level(toId(user));
 		return Math.floor(Math.pow(lvl, multiply) * minLevelExp) - curExp;
 	}
 	
