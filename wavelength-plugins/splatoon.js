0c8c7d9a06c620d228fa4deda5542c234290d5b6
Insist <DeathlyPlays@users.noreply.github.com>
2018-02-26 10:25:43 -0600

    Splatoon: Implement Ranks, Re-write Profile, Refactor Ranks into a singular array at the top of the file, etc

--- wavelength-plugins/splatoon.js
+++ wavelength-plugins/splatoon.js
@@ -6,7 +6,11 @@
 
 "use strict";
 
+let ranks = ["C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S", "S+"];
 let weapons = [".52 Gal", ".52 Gal Deco", ".96 Gal", "Aerospray MG", "Aerospray RG", "Bamboozler 14 Mk I", "Bamboozler 14 Mk II", "Blaster", "Carbon Roller", "Clash Blaster", "Clash Blaster Neo", "Classic Squiffer", "Custom Blaster", "Custom E-Liter 4K", "Custom E-Liter 4K Scope", "Custom Goo Tuber", "Custom Hydra Splatling", "Custom Jet Squelcher", "Custom Range Blaster", "Custom Splattershot Jr.", "Dapple Dualies", "Dapple Dualies Nouveau", "Dark Tetra Dualies", "Dualie Squelchers", "Dynamo Roller", "E-Liter 4K", "E-Liter 4K Scope", "Enperry Splat Dualies", "Firefin Splat Charger", "Firefine Splatterscope", "Flingza Roller", "Foil Flingza Roller", "Forge Splattershot Pro", "Foil Squeezer", "Glooga Dualies", "Glooga Dualies Deco", "Gold Dynamo Roller", "Goo Tuber", "H-3 Nozzlenose", "Heavy Splatling", "Heavy Splatling Deco", "Hero Blaster Replica", "Hero Brella Replica", "Hero Charger Replica", "Hero Dualie Replica", "Hero Roller Replica", "Hero Shot Replica", "Hero Slosher Replica", "Hero Splatling Replica", "Herobrush Replica", "Hydra Splatling", "Inkbrush", "Inkbrush Nouveau", "Jet Squelcher", "Krak-On Splat Roller", "L-3 Nozzlenose", "L-3 Nozzlenose D", "Light Tetra Dualies", "Luna Blaster", "Luna Blaster Neo", "Mini Splatling", "Neo Splash-o-matic", "Neo Sploosh-o-matic", "New Squiffer", "N-ZAP '85", "N-ZAP '89", "Octobrush", "Octobrush Nouveau", "Range Blaster", "Rapid Blaster", "Rapid Blaster Deco", "Rapid Blaster Pro", "Slosher", "Slosher Deco", "Sloshing Machine", "Sloshing Machine Neo", "Sorella Brella", "Splash-o-matic", "Splat Brella", "Splat Charger", "Splat Dualies", "Splat Roller", "Splatterscope", "Splattershot", "Splattershot Jr.", "Splattershot Pro", "Sploosh-o-matic", "Squeezer", "Tenta Brella", "Tentatek Splattershot", "Tri-Slosher", "Tri-Slosher Nouveau", "Undercover Brella", "Zink Mini Splatling"];
+let headgears = ["18K Aviators", "Annaki Beret", "Annaki Mask", "Armor Helmet Replica", "Backwards Cap", "Bamboo Hat", "B-ball Headband", "Bike Helmet", "Black Arrowbands", "Blowfish Bell Hat", "Bobble Hat", "Bucket Hat", "Camo Mesh", "Camping Hat", "Classic Straw Boater", "Cycle King Cap", "Cycling Hat", "Designer Headphones", "Double Egg Shades", "Dust Blocker 2000", "Eminence Cuff", "Face Visor", "Fake Contacts", "Firefin Facemask", "FishFry Biscuit Bandana", "FishFry Visor", "Five-Panel Cap", "Forge Mask", "Fugu Bell Hat", "Full Moon Glasses", "Half-Rim Glasses", "Headlamp Helmet", "Hero Headphones Replica", "Hero Headset Replica", "Hickory Work Cap", "Hockey Helmet", "House-Tag Denim Cap", "Jellyvader Cap", "Jet Cap", "Jungle Hat", "King Facemask", "King Flip Mesh", "Knitted Hat", "Lightweight Cap", "Matte Bike Helmet", "Moist Ghillie Helmet", "Motocross Nose Guard", "MTB Helmet", "Noise Cancelers", "Paintball Mask", "Painter's Mask", "Paisley Bandana", "Patched Hat", "Pilot Glasses", "Power Mask", "Power Mask Mark I", "Retro Specs", "Safari Hat", "Samurai Helmet", "Short Beanie", "Skate Helmet", "Skull Bandana", "Sneaky Beanie", "Snorkel Mask", "Soccer Headband", "Special Forces Beret", "Splash Goggles", "Sporty Bobble Hat", "Squash Headband", "Squid Clip-ons", "Squid Facemask", "Squid Hairclip", "Squidfin Hook Cans", "Squidlife Headphones", "Squidvader Cap", "Squinja Mask", "Stealth Goggles", "Straw Boater", "Streetstyle Cap", "Striped Beanie", "Studio Headphones", "Sun Visor", "Takoroka Mesh", "Takoroka Visor", "Tennis Headband", "Tinted Shades", "Treasure Hunter", "Tulip Parasol", "Two-Stripe Mesh", "Urchins Cap", "Visor Skate Helmet", "Welding Mask", "White Headband", "Woolly Urchens Classic", "Yamagiri Beanie"];
+let shirts = ["Aloha Shirt", "Anchor Sweat", "Armor Jacket Replica", "B-ball Jersey (Away)", "B-ball Jersey (Home)", "Baby-Jelly Shirt", "Baseball Jersey", "Basic Tee", "Berry Ski Jacket", "Black 8-Bit FishFry", "Black Anchor Tee", "Black Baseball LS", "Black Inky Rider", "Black Layered LS", "Black LS", "Black Pipe Tee", "Black Polo", "Black Squideye", "Black Tee", "Blue Peaks Tee", "Blue Peaks Tee", "Blue Sailor Suit", "Camo Layered LS", "Camo Zip Hoodie", "Carnivore Tee", "Choco Layered LS", "CoroCoro Hoodie", "Cycle King Jersey", "Cycling Shirt", "Dark Urban Vest", "FC Albacore", "Firefin Navy Sweat", "Forest Vest", "Forge Inkling Parka", "Forge Octarian Jacket", "Fugu Tee", "Grape Tee", "Gray College Sweat", "Gray Mixed Shirt", "Gray Vector Tee", "Green Cardigan", "Green Striped LS", "Green Tee", "Green Zip Hoodie", "Green-Check Shirt", "Herbivore Tee", "Hero Jacket Replica", "Ivory Peaks Tee", "Krak-On 528", "Layered Anchor LS", "Layered Vector LS", "Linen Shirt", "Logo Aloha Shirt", "Lumberjack Shirt", "Mint Tee", "Mountain Vest", "Navy College Sweat", "Navy Striped LS", "Octo Tee", "Octoling Armor", "Olive Ski Jacket", "Orange Cardigan", "Part-Time Pirate", "Pearl Tee", "Pirate-Stripe Tee", "Power Armor", "Purple Camo LS", "Rainy-Day Tee", "Red Vector Tee", "Red-Check Shirt", "Reel Sweat", "Reggae Tee", "Retro Gamer Sweat", "Retro Sweat", "Rockenberg Black", "Rockenberg White", "Rodeo Shirt", "Round-Collar Shirt", "Sage Polo", "Sailor-Stripe Tee", "Samurai Jacket", "School Jersey", "School Uniform", "Shirt & Tie", "Shrimp-Pink Polo", "Sky-Blue Squideye", "Slipstream United", "Splatfest Tee", "SQUID GIRL Tunic", "Squid Satin Jacket", "Squid-Pattern Waistcoat", "Squid-Stitch Tee", "Squidmark LS", "Squidmark Sweat", "Squidstar Waistcoat", "Striped Peaks LS", "Striped Rugby", "Striped Shirt", "Sunny-Day Tee", "Traditional Apron", "Tricolor Rugby", "Urchins Jersey", "Varsity Baseball LS", "Varsity Jacket", "Vintage Check Shirt", "White 8-Bit FishFry", "White Anchor Tee", "White Baseball LS", "White Inky Rider", "White Layered LS", "White Line Tee", "White LS", "White Sailor Suit", "White Shirt", "White Striped LS", "White Tee", "Yellow Layered LS", "Yellow Urban Vest", "Zapfish Satin Jacket", "Zekko Baseball LS", "Zekko Hoodie", "Zink Layered LS", "Zink LS"];
+let shoes = ["Acerola Rain Boots", "Amber Sea Slug Hi-Tops", "Angry Rain Boots", "Annaki Habaneros", "Armor Boot Replicas", "Arrow Pull-Ons", "Birch Climbing Shoes", "Black Dakroniks", "Black Flip-Flops", "Black Norimaki 750s", "Black Seahorses", "Black Trainers", "Blue & Black Squidkid IV", "Blue Laceless Dakroniks", "Blue Lo-Tops", "Blue Moto Boots", "Blue Slip-Ons", "Blueberry Casuals", "Bubble Rain Boots", "Canary Trainers", "Cherry Kicks", "Choco Clogs", "Clownfish Basics", "Crazy Arrows", "Cream Basics", "Cyan Trainers", "Deepsea Leather Boots", "Fringed Loafers", "Gold Hi-Horses", "Gray Sea-Slug Hi-Tops", "Gray Yellow-Soled Wingtips", "Green Iromaki 750s", "Green Laceups", "Green Rain Boots", "Hero Runner Replicas", "Hero Snowboots Replicas", "Hunter Hi-Tops", "Hunting Boots", "Kid Clams", "LE Soccer Shoes", "Luminous Delta Straps", "Mawcasins", "Milky Enperrials", "Mint Dakroniks", "Moist Ghillie Boots", "Moto Boots", "Navy Enperrials", "Navy Red-Soled Wingtips", "Neon Delta Straps", "Neon Sea Slugs", "N-Pacer Ag", "N-Pacer Au", "Orange Arrows", "Orca Hi-Tops", "Orca Woven Hi-Tops", "Oyster Clogs", "Pink Trainers", "Piranha Moccasins", "Plum Casuals", "Polka-dot Slip-Ons", "Power Boots", "Power Boots Mk I", "Pro Trail Boots", "Punk Blacks", "Punk Cherries", "Punk Whites", "Purple Hi-Horses", "Purple Iromaki 750s", "Purple Sea Slugs", "Red & Black Squidkid IV", "Red FishFry Sandals", "Red Hi-Horses", "Red Hi-Tops", "Red Iromaki 750s", "Red Sea Slugs", "Red Slip-Ons", "Red-Mesh Sneakers", "Roasted Brogues", "Samurai Shoes", "School Shoes", "Shark Moccasins", "Smoky Wingtips", "Snow Delta Straps", "Snowy Down Boots", "Soccer Shoes", "	Squid-Stitch Slip-Ons", "Squinja Boots", "Squink Wingtips", "Strapping Reds", "Strapping Whites", "Sun & Shade Squidkid IV", "Sunny Climbing Shoes", "Sunset Orca Hi-Tops", "Tan Work Boots", "Trail Boots", "Turquoise Kicks", "Violet Trainers", "White Arrows", "White Kicks", "White Laceless Dakroniks", "White Norimaki 750s", "White Seahorses", "Yellow Iromaki 750s", "Yellow-Mesh Sneakers", "Zombie Hi-Horses"];
 
 exports.commands = {
 	"spl2n": "splatoon",
@@ -17,10 +21,10 @@ exports.commands = {
 		rank: {
 			clamblitz: "cb",
 			cb: function (target, room, user) {
-				if (!target) return this.parse("/splatoonhelp");
+				if (!target) return this.parse("/splatrankshelp");
 				target = target.trim().toUpperCase();
 				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
-				if (!["C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S", "S+"].includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
+				if (!ranks.includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
 				splatProfile.ranks.cb = target;
 				Db.splatoon.set(user.userid, splatProfile);
 				return this.sendReply(`You have successfully set your Clam Blitz ranking to "${target}".`);
@@ -28,10 +32,10 @@ exports.commands = {
 
 			rainmaker: "rm",
 			rm: function (target, room, user) {
-				if (!target) return this.parse("/splatoonhelp");
+				if (!target) return this.parse("/splatrankshelp");
 				target = target.trim().toUpperCase();
 				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
-				if (!["C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S", "S+"].includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
+				if (!ranks.includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
 				splatProfile.ranks.rm = target;
 				Db.splatoon.set(user.userid, splatProfile);
 				return this.sendReply(`You have successfully set your Rainmaker ranking to "${target}".`);
@@ -40,10 +44,10 @@ exports.commands = {
 			splatzones: "sz",
 			splatzone: "sz",
 			sz: function (target, room, user) {
-				if (!target) return this.parse("/splatoonhelp");
+				if (!target) return this.parse("/splatrankshelp");
 				target = target.trim().toUpperCase();
 				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
-				if (!["C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S", "S+"].includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
+				if (!ranks.includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
 				splatProfile.ranks.sz = target;
 				Db.splatoon.set(user.userid, splatProfile);
 				return this.sendReply(`You have successfully set your Splat Zones ranking to "${target}".`);
@@ -51,10 +55,10 @@ exports.commands = {
 
 			towercontrol: "tc",
 			tc: function (target, room, user) {
-				if (!target) return this.parse("/splatoonhelp");
+				if (!target) return this.parse("/splatrankshelp");
 				target = target.trim().toUpperCase();
 				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
-				if (!["C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S", "S+"].includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
+				if (!ranks.includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
 				splatProfile.ranks.tc = target;
 				Db.splatoon.set(user.userid, splatProfile);
 				return this.sendReply(`You have successfully set your Tower Control ranking to "${target}".`);
@@ -62,7 +66,7 @@ exports.commands = {
 
 			salmonrun: "sr",
 			sr: function (target, room, user) {
-				if (!target) return this.parse("/splatoonhelp");
+				if (!target) return this.parse("/splatrankshelp");
 				target = target.trim().split("-").map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()).join("-");
 				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
 				if (!["Intern", "Apprentice", "Part-Timer", "Go-Getter", "Overachiever", "Profreshional"].includes(target)) return this.errorReply(`Invalid Ranking; check your spelling?`);
@@ -86,6 +90,7 @@ exports.commands = {
 			return this.sendReply(`Your Splatoon 2 weapon has been set to "${target}".`);
 		},
 
+		"!randomweapon": true,
 		randweapon: "randomweapon",
 		randomweapon: function (target, room, user) {
 			if (!this.runBroadcast()) return;
@@ -102,7 +107,7 @@ exports.commands = {
 				if (room.id !== "splatoon") return this.errorReply(`This command only works in the Splatoon room.`);
 				let SPLATFEST = Db.splatoon.get("SPLATFEST", {alpha: null, bravo: null, active: false});
 				let [team1, team2] = target.split(",").map(p => p.trim());
-				if (!(team1 && team2)) return this.parse("/splatoonhelp");
+				if (!(team1 && team2)) return this.parse("/splatfesthelp");
 				if (SPLATFEST.active) return this.errorReply(`Splatfest is already active.`);
 				SPLATFEST.active = true;
 				if (Rooms("splatoon")) {
@@ -130,6 +135,10 @@ exports.commands = {
 						delete splatProfile.splatfest;
 						Db.splatoon.set(u, splatProfile);
 					}
+					if (splatProfile.clothing === "Splatfest Tee") {
+						delete splatProfile.clothing;
+						Db.splatoon.set(u, splatProfile);
+					}
 				}
 				if (Rooms("splatoon")) {
 					Rooms("splatoon").addRaw(`${WL.nameColor(user.name, true)} has disabled Splatfest.`);
@@ -140,7 +149,7 @@ exports.commands = {
 			setteam: "join",
 			jointeam: "join",
 			join: function (target, room, user) {
-				if (!target) return this.parse(`/splatoonhelp`);
+				if (!target) return this.parse(`/splatfesthelp`);
 				let SPLATFEST = Db.splatoon.get("SPLATFEST", {alpha: null, bravo: null, active: false});
 				if (!SPLATFEST.active) return this.errorReply(`There is currently not a Splatfest. :(`);
 				if (toId(SPLATFEST.alpha) !== toId(target) && toId(SPLATFEST.bravo) !== toId(target)) return this.errorReply(`This is not a Splatfest team.`);
@@ -150,6 +159,7 @@ exports.commands = {
 				return this.sendReply(`You have successfully joined Splatfest Team "${target}".`);
 			},
 
+			"!team": true,
 			teams: "team",
 			team: function (target, room, user) {
 				if (!this.runBroadcast()) return;
@@ -196,6 +206,47 @@ exports.commands = {
 			return this.sendReply(`Your Prestige has been set to: Prestige ${target}.`);
 		},
 
+		gear: {
+			hat: "headgear",
+			cap: "headgear",
+			headgear: function (target, room, user) {
+				if (!target) return this.parse(`/splatgearhelp`);
+				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
+				if (!headgears.includes(target)) return this.errorReply(`Invalid headgear; check your spelling? [case sensitive]`);
+				splatProfile.headgear = target;
+				Db.splatoon.set(user.userid, splatProfile);
+				return this.sendReply(`Your Splatoon 2 headgear has been set to "${target}".`);
+			},
+
+			shirt: "clothing",
+			tee: "clothing",
+			clothing: function (target, room, user) {
+				if (!target) return this.parse(`/splatgearhelp`);
+				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
+				let SPLATFEST = Db.splatoon.get("SPLATFEST", {alpha: null, bravo: null, active: false});
+				if (!shirts.includes(target)) return this.errorReply(`Invalid shirt; check your spelling? [case sensitive]`);
+				if (target === "Splatfest Tee" && !SPLATFEST.active && !splatProfile.splatfest) return this.errorReply(`Your Splatfest Tee has been returned, since there is no longer a Splatfest or you haven't joined a Splatfest team yet.`)
+				splatProfile.clothing = target;
+				Db.splatoon.set(user.userid, splatProfile);
+				return this.sendReply(`Your Splatoon 2 shirt has been set to "${target}".`);
+			},
+
+			shoes: function (target, room, user) {
+				if (!target) return this.parse(`/splatgearhelp`);
+				let splatProfile = Db.splatoon.get(user.userid, {ranks: {}});
+				if (!shoes.includes(target)) return this.errorReply(`Invalid shoe; check your spelling? [case sensitive]`);
+				splatProfile.shoes = target;
+				Db.splatoon.set(user.userid, splatProfile);
+				return this.sendReply(`Your Splatoon 2 shoes has been set to "${target}".`);
+			},
+
+			"": "help",
+			help: function () {
+				this.parse(`/splatgearhelp`);
+			},
+		},
+
+		"!profile": true,
 		profile: function (target, room, user) {
 			if (!this.runBroadcast()) return;
 			if (!target) target = user.userid;
@@ -211,30 +262,17 @@ exports.commands = {
 			if (splatProfile.level) profile += ` <strong>Level:</strong> ${splatProfile.level}`;
 			if (splatProfile.prestige) profile += ` <strong>Prestige:</strong> ${splatProfile.prestige}`;
 			profile += `<br />`;
-			if (Db.switchfc.has(toId(username))) {
-				profile += `<strong>Switch Friend Code:</strong> SW-${Db.switchfc.get(toId(username))}<br />`;
-			}
-			if (splatProfile.weapon) {
-				profile += `<strong>Weapon:</strong> ${splatProfile.weapon}<br />`;
-			}
-			if (splatProfile.splatfest && SPLATFEST.active) {
-				profile += `<strong>Splatfest Team:</strong> ${splatProfile.splatfest}<br />`;
-			}
-			if (splatProfile.ranks.cb) {
-				profile += `<strong>Clam Blitz:</strong> ${splatProfile.ranks.cb}<br />`;
-			}
-			if (splatProfile.ranks.rm) {
-				profile += `<strong>Rainmaker:</strong> ${splatProfile.ranks.rm}<br />`;
-			}
-			if (splatProfile.ranks.sz) {
-				profile += `<strong>Splat Zones:</strong> ${splatProfile.ranks.sz}<br />`;
-			}
-			if (splatProfile.ranks.tc) {
-				profile += `<strong>Tower Control:</strong> ${splatProfile.ranks.tc}<br />`;
-			}
-			if (splatProfile.ranks.sr) {
-				profile += `<strong>Salmon Run:</strong> ${splatProfile.ranks.sr}<br />`;
-			}
+			if (Db.switchfc.has(toId(username))) profile += `<strong>Switch Friend Code:</strong> SW-${Db.switchfc.get(toId(username))}<br />`;
+			if (splatProfile.weapon) profile += `<strong>Weapon:</strong> ${splatProfile.weapon}<br />`;
+			if (splatProfile.headgear) profile += `<strong>Headgear:</strong> ${splatProfile.headgear}<br />`;
+			if (splatProfile.clothing) profile += `<strong>Clothing:</strong> ${splatProfile.clothing}<br />`;
+			if (splatProfile.shoes) profile += `<strong>Shoes:</strong> ${splatProfile.shoes}<br />`;
+			if (splatProfile.splatfest && SPLATFEST.active) profile += `<strong>Splatfest Team:</strong> ${splatProfile.splatfest}<br />`;
+			if (splatProfile.ranks.cb) profile += `<strong>Clam Blitz:</strong> ${splatProfile.ranks.cb}<br />`;
+			if (splatProfile.ranks.rm) profile += `<strong>Rainmaker:</strong> ${splatProfile.ranks.rm}<br />`;
+			if (splatProfile.ranks.sz) profile += `<strong>Splat Zones:</strong> ${splatProfile.ranks.sz}<br />`;
+			if (splatProfile.ranks.tc) profile += `<strong>Tower Control:</strong> ${splatProfile.ranks.tc}<br />`;
+			if (splatProfile.ranks.sr)  profile += `<strong>Salmon Run:</strong> ${splatProfile.ranks.sr}<br />`;
 			profile += `</div>`;
 			this.sendReplyBox(profile);
 		},
@@ -255,6 +293,9 @@ exports.commands = {
 		/splatoon splatfest end - Ends the Splatfest. Requires @, &, #, ~.
 		/splatoon splatfest join [Splatfest team name] - Joins the specified Splatfest team.
 		/splatoon splatfest teams - Shows the Splatfest teams.
+		/splatoon gear headgear [headgear] - Sets your Splatoon 2 Headgear.
+		/splatoon gear clothing [clothing] - Sets your Splatoon 2 Clothing.
+		/splatoon gear shoes [shoes] - Sets your Splatoon 2 Shoes.
 		/splatoon randomweapon - Sends a random weapon from Splatoon 2 into chat.
 		/splatoon profile [optional target] - Displays the specified user's Splatoon 2 Profile. Defaults to yourself.`,
 	],
@@ -268,5 +309,11 @@ exports.commands = {
 		/splatoon splatfest end - Ends the Splatfest. Requires @, &, #, ~.
 		/splatoon splatfest join [Splatfest team name] - Joins the specified Splatfest team.
 		/splatoon splatfest teams - Shows the Splatfest teams.`,
+	],
+
+	splatgearhelp: [
+		`/splatoon gear headgear [headgear] - Sets your Splatoon 2 Headgear.
+		/splatoon gear clothing [clothing] - Sets your Splatoon 2 Clothing.
+		/splatoon gear shoes [shoes] - Sets your Splatoon 2 Shoes.`,
 	],
 };
