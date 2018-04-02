6d4e181597e1e24d139143dd5170e580ff8d8a9e
Insist <DeathlyPlays@users.noreply.github.com>
2018-04-02 13:40:20 -0500

    Last Active: Remove showing msg

--- wavelength-plugins/profile.js
+++ wavelength-plugins/profile.js
@@ -546,7 +546,7 @@ exports.commands = {
 		if (!this.runBroadcast()) return;
 		let targetUser = Users.get(toId(target));
 		if (!targetUser || !targetUser.connected) return this.errorReply(`${target} is not online. Use /seen to find out how long ago they left.`);
-		return this.sendReplyBox(`${WL.nameColor(targetUser, true, true)} was last active <strong>${Chat.toDurationString(Date.now() - targetUser.lastPublicMessage)} ago</strong> saying "${targetUser.lastPublicMessage}".`);
+		return this.sendReplyBox(`${WL.nameColor(targetUser, true, true)} was last active <strong>${Chat.toDurationString(Date.now() - targetUser.lastPublicMessage)} ago.</strong>`);
 	},
 	lastactivehelp: ["/lastactive - Shows how long ago it has been since a user has posted a message."],
 
