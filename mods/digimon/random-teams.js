0d1226977e5f51b55235812b856d643987e1e533
AlfaStorm <juancol1@hotmail.com>
2018-05-22 19:47:00 -0400

    Digimon Showdown - Fix Spelling Mistakes (#207)

--- mods/digimon/random-teams.js
+++ mods/digimon/random-teams.js
@@ -91,8 +91,8 @@ class RandomDigimonTeams extends RandomTeams {
 				baseSignatureMove: "pepperbreath",
 				signatureMove: "Pepper Breath",
 			},
-			"Aruramon": {
-				species: "Aruramon",
+			"Aruraumon": {
+				species: "Aruraumon",
 				ability: "Virus",
 				moves: ['charmperfume', 'rootbind', 'venomdisaster', 'waterblitz', 'superstinkyjet', 'shadowfall', 'blackout'],
 				baseSignatureMove: "nemesisivy",
@@ -238,8 +238,8 @@ class RandomDigimonTeams extends RandomTeams {
 				baseSignatureMove: "puppyhowl",
 				signatureMove: "Puppy Howl",
 			},
-			"Shamamon": {
-				species: "Shamamon",
+			"Shamanmon": {
+				species: "Shamanmon",
 				ability: "Virus",
 				moves: ['charmperfume', 'rootbind', 'rockfall', 'musclecharge', 'warcry', 'reboundstrike', 'megatonpunch', 'shadowfall', 'pooptoss'],
 				baseSignatureMove: "dancingbone",
@@ -856,22 +856,22 @@ class RandomDigimonTeams extends RandomTeams {
 				baseSignatureMove: "modestlystun",
 				signatureMove: "Modestly Stun",
 			},
-			"MeicrackmonVM": {
+			"MeicrackmonViciousMode": {
 				name: "Meicrackmon",
-				species: "meicrackmonvm",
+				species: "meicrackmonviciousmode",
 				ability: "Virus",
 				moves: ['warcry', 'windcutter', 'fightingaura', 'megatonpunch', 'mechanicalclaw', 'venomdisaster', 'shadowfall', 'evilsquall', 'blackout'],
 				baseSignatureMove: "berserkthinking",
 				signatureMove: "Berserk Thinking",
 			},
-			"MetalGreymon": {
-				species: "metalgreymon",
+			"MetalGreymonVaccine": {
+				species: "metalgreymonvaccine",
 				ability: "Vaccine",
 				moves: ['burningheart', 'heatbreath', 'firetower', 'infinityburn', 'musclecharge', 'sonicjab', 'reboundstrike', 'busterdrive', 'mechanicalclaw', 'gigawattlaser', 'deleteprogram'],
 				baseSignatureMove: "gigadestroyer",
 				signatureMove: "Giga Destroyer",
 			},
-			"MetalGreymonV": {
+			"MetalGreymonVIrus": {
 				name: "MetalGreymon",
 				species: "metalgreymonvirus",
 				ability: "Virus",
@@ -1013,16 +1013,16 @@ class RandomDigimonTeams extends RandomTeams {
 				baseSignatureMove: "tomahawkstinger",
 				signatureMove: "Tomahawk Stinger",
 			},
-			"Cherubimon": {
-				species: "Cherubimonevil",
+			"CherubimonEvil": {
+				species: "CherubimonEvil",
 				ability: "Virus",
 				moves: ['wingshoes', 'thunderjustice', 'megalospark', 'megatonpunch', 'blackout', 'chaoscloud', 'shadowfall', 'evilsquall', 'dgdimension', 'gigawattlaser'],
 				baseSignatureMove: "lightningspear",
 				signatureMove: "Lightning Spear",
 			},
-			"CherubimonG": {
+			"CherubimonGood": {
 				name: "Cherubimon",
-				species: "Cherubimongood",
+				species: "CherubimonGood",
 				ability: "Vaccine",
 				moves: ['wingshoes', 'thunderjustice', 'megalospark', 'megatonpunch', 'holybreath', 'saintray', 'holyjudgment', 'shiningnova', 'dgdimension', 'gigawattlaser'],
 				baseSignatureMove: "heavensjudgment",
