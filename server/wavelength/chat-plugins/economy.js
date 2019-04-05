4dcae63e82d155ee86ab3ce6cb3aa3228326205f
Dragotic <pokemon.ahmed@gmail.com>
2019-04-05 09:37:30 +0000

    WL Economy: Mop earlier mess-up

--- server/wavelength/chat-plugins/economy.js
+++ server/wavelength/chat-plugins/economy.js
@@ -15,7 +15,7 @@ exports.commands = {
 			let [targetU, value] = target.split(',');
 			const targetUser = toId(targetU);
 			if (!targetUser || !value) return this.parse('/help stardust');
-			if (targetU.length > 18) return this.errorReply('Usernames must be 18 characters or shorter');
+			if (targetUser.length > 18) return this.errorReply('Usernames must be 18 characters or shorter');
 
 			const amountGiven = econWL.award(targetUser, value);
 			if (!amountGiven) return this.errorReply('Indefinite amount; use whole numbers');
@@ -30,7 +30,7 @@ exports.commands = {
 			let [targetU, value] = target.split(',');
 			const targetUser = toId(targetU);
 			if (!targetUser || !value) return this.parse('/help stardust');
-			if (targetU.length > 18) return this.errorReply('Usernames must be 18 characters or shorter');
+			if (targetUser.length > 18) return this.errorReply('Usernames must be 18 characters or shorter');
 			if (econWL.get(targetU) < value) return this.errorReply(`You can't take more ${econWL.getCurrencyLabel(value, false)} than they have!`);
 
 			const amountLeft = econWL.remove(targetUser, value);
@@ -47,7 +47,7 @@ exports.commands = {
 			const targetUser = toId(targetU);
 			if (!targetUser || !value) return this.parse('/help stardust');
 			if (user.userid === targetUser) return this.errorReply(`You can't transfer ${econWL.getCurrencyLabel(value, false)} to yourself!`);
-			if (targetU.length > 18) return this.errorReply('Usernames must be 18 characters or shorter');
+			if (targetUser.length > 18) return this.errorReply('Usernames must be 18 characters or shorter');
 			if (econWL.get(user.userid) < value) return this.errorReply(`You can't transfer more ${econWL.getCurrencyLabel(value, false)} than you have!`);
 
 			const amountLeft = econWL.remove(user.userid, value);
