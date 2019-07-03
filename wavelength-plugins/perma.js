ada1b86a918c711d004bee756e4a4e8861d2b947
Acquiescence3 <52384581+Acquiescence3@users.noreply.github.com>
2019-07-03 14:57:33 -0500

    Update perma.js
    In trying to fix typos, I accidentally made a new typo, which I have now fixed.

--- wavelength-plugins/perma.js
+++ wavelength-plugins/perma.js
@@ -37,7 +37,7 @@ exports.commands = {
 		if (tarUser.trusted && cmd !== 'forcepermalock') return this.errorReply(tarUser.name + ' is a trusted user. If your sure you want to permalock them, please use /forcepermalock');
 		Db.userType.set(tarUser.userid, 5);
 		if (!Punishments.userids.get(tarUser.userid) || Punishments.userids.get(tarUser.userid)[0] !== 'BAN') Punishments.lock(tarUser, Date.now() + (1000 * 60 * 60 * 24 * 30), tarUser.userid, `Permalocked as ${tarUser.userid}`);
-		tarUser.popup('You have been permalocked by ' + user.name + '.\nUnlike permalocks issued by the main server, this permalock only eaffcts this server.');
+		tarUser.popup('You have been permalocked by ' + user.name + '.\nUnlike permalocks issued by the main server, this permalock only affects this server.');
 		if (tarUser.trusted) Monitor.log('[CrisisMonitor] Trusted user ' + tarUser.userid + ' was permalocked by ' + user.name + ' and was automatically demoted from ' + tarUser.distrust() + '.');
 		if (Rooms('upperstaff')) Monitor.adminlog('[PermaMonitor] ' + user.name + ' has permalocked ' + tarUser.name + '.');
 		this.globalModlog("PERMALOCK", tarUser, " by " + user.name);
